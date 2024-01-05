23a032e7bdfc726c7a3207ee9a86acebdb3f4442
am121-dwc <aoimorisugi121@gmail.com>
2024-01-05 05:29:24 +0000

    model ok

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2023_12_23_103935) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
-    t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
